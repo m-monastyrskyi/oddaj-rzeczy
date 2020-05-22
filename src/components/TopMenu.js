32931a64af588822e7b0cb3245fcdf6282c1a8be
Mykola Monastyrskyi <kolyan4ik@gmail.com>
2020-05-22 17:38:39 +0200

    add three columns section

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -3,27 +3,28 @@ import {Link} from "react-router-dom";
 
 const TopMenu = () => {
     return (
+        <div className="menu-wrapper">
+            <div className="container">
+                <div className="top-menu">
+                    <div className="user-menu">
+                        <Link className="user-menu__item" to='/logowanie/'>Zaloguj</Link>
+                        <Link className="user-menu__item accent" to='/rejestracja/'>Załóż konto</Link>
+                    </div>
 
-        <div className="container">
-            <div className="top-menu">
-                <div className="user-menu">
-                    <Link className="user-menu__item" to='#'>Zaloguj</Link>
-                    <Link className="user-menu__item accent" to='#'>Załóż konto</Link>
+                    <nav className="navigation">
+                        <ul className="navigation__list">
+                            <li className="navigation__item"><Link className="navigation__link active"
+                                                                   to='#'>Start</Link></li>
+                            <li className="navigation__item"><Link className="navigation__link" to='#'>O co
+                                chodzi?</Link></li>
+                            <li className="navigation__item"><Link className="navigation__link" to='#'>O nas</Link></li>
+                            <li className="navigation__item"><Link className="navigation__link" to='#'>Fundacja i
+                                organizacje</Link></li>
+                            <li className="navigation__item"><Link className="navigation__link" to='#'>Kontakt</Link>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
-
-                <nav className="navigation">
-                    <ul className="navigation__list">
-                        <li className="navigation__item"><Link className="navigation__link active"
-                                                               to='#'>Start</Link></li>
-                        <li className="navigation__item"><Link className="navigation__link" to='#'>O co
-                            chodzi?</Link></li>
-                        <li className="navigation__item"><Link className="navigation__link" to='#'>O nas</Link></li>
-                        <li className="navigation__item"><Link className="navigation__link" to='#'>Fundacja i
-                            organizacje</Link></li>
-                        <li className="navigation__item"><Link className="navigation__link" to='#'>Kontakt</Link>
-                        </li>
-                    </ul>
-                </nav>
             </div>
         </div>
     );

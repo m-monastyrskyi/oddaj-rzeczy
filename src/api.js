9464e3ace423bf129c5fa78a613c626602f87716
Mykola Monastyrskyi <kolyan4ik@gmail.com>
2020-05-25 17:59:38 +0200

    add footer + contact form

---
+++ src/api.js
@@ -0,0 +1,29 @@
+const validate = (name, email, message) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const errors = {};
+    if (!name) {
+        errors.name = 'Wymagane';
+    } else if (name.indexOf(" ") >= 0) {
+        errors.name = 'Podane imię jest nieprawidłowe!';
+    }
+
+    if (!email) {
+        errors.email = 'Wymagane';
+    } else if (!re.test(String(email).toLowerCase())) {
+        errors.email = 'Podany email jest nieprawidłowy';
+    }
+
+    if (!message) {
+        errors.message = 'Wymagane';
+    } else if (message.length < 120) {
+        errors.message = 'Wiadomość musi mieć conajmniej 120 znaków!';
+    }
+    if (!errors.name && !errors.email && !errors.message) {
+        errors.ok = true;
+    }
+    return errors;
+}
+
+export {
+    validate
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,17 @@ import Header from "./Header";
 import TopMenu from "./TopMenu";
 import ThreeColumns from "./ThreeColumns";
 import SimpleSteps from "./SimpleSteps";
+//import FooterFormik from "./FooterFormik";
+import FooterForm from "./FooterForm";
 
 const Home = () => {
     return (
         <>
-            <TopMenu />
-            <Header />
-            <ThreeColumns />
-            <SimpleSteps />
+            <TopMenu/>
+            <Header/>
+            <ThreeColumns/>
+            <SimpleSteps/>
+            <FooterForm />
         </>
     );
 };

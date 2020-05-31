7176a9577fe8b2c1b842ba83653cc84c60884135
Mykola Monastyrskyi <kolyan4ik@gmail.com>
2020-05-31 11:25:11 +0200

    minor changes

--- src/components/Survey/Survey.js
+++ src/components/Survey/Survey.js
@@ -1,14 +1,20 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import TopMenu from "../TopMenu";
 import FooterForm from "../FooterForm";
 import SurveyHeader from "./SurveyHeader";
 import SurveyForm from "./SurveyForm";
 import {GlobalContext} from "../GlobalContext";
 import {Redirect} from "react-router-dom";
+import { animateScroll as scroll } from 'react-scroll'
+
 
 
 const Survey = () => {
     const [user] = useContext(GlobalContext);
+
+    useEffect(()=>{
+        scroll.scrollTo(0);
+    },[])
 
     if (!user) {
         return (
